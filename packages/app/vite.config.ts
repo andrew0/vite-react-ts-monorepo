5e0c465779c3da7cd646a5957aef24cff6fa07ce
toub <nicolas@toublanc.org>
2021-03-04 15:02:19 +0100

    feat: updade to vite v2 #1

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,17 +1,21 @@
-import * as reactPlugin from 'vite-plugin-react'
-import type { UserConfig } from 'vite'
-import path from 'path'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from "path";
+import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-  jsx: 'react',
-  plugins: [reactPlugin],
+  plugins: [reactRefresh()],
   optimizeDeps: {
-    link: ['vite-react-ts-components'],
+    entries: ["vite-react-ts-components"],
   },
-  alias: {
-    'vite-react-ts-components': '/@linked/vite-react-ts-components/index.ts',
-    '/@linked/vite-react-ts-components/': path.resolve(require.resolve('vite-react-ts-components/package.json'), '../src')
-  }
-}
+  resolve: {
+    alias: {
+      "vite-react-ts-components": "/@linked/vite-react-ts-components/index.ts",
+      "/@linked/vite-react-ts-components": path.resolve(
+        require.resolve("vite-react-ts-components/package.json"),
+        "../src"
+      ),
+    },
+  },
+};
 
-export default config
+export default config;
